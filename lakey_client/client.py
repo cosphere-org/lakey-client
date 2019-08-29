55a1b25f61103ebdd17137bc134b1dea545ad7c1
skpk <stefan.freespace@gmail.com>
2019-08-29 09:43:30 +0200

    Add html to estimate size and cut df to spec

--- lakey_client/client.py
+++ lakey_client/client.py
@@ -4,6 +4,16 @@ import os
 
 import requests
 import pandas as pd
+import operator
+
+
+operators_to_def = {
+    '<': operator.lt,
+    '<=': operator.le,
+    '>': operator.gt,
+    '>=': operator.ge,
+    '=': operator.eq,
+}
 
 
 class LakeyClient:
@@ -26,8 +36,11 @@ class LakeyClient:
                 'catalogue_item_name': name
             })
 
-        # TODO: make it to MB, KB etc. and wrap into HTML
-        return response.json()['estimated_size']
+        size = int(response.json()['estimated_size']) / 1000000
+        return (f'<div style="display: block; width: 100%; height: 100px; '
+                f'background-color: blue; text-align: center;"> '
+                f'<b style="color: white;">{size} MB</b> '
+                f'</div>')
 
     def download(self, name, spec):
 
@@ -47,11 +60,13 @@ class LakeyClient:
         for chunk in response.json()['chunks']:
             chunk_df = pd.read_parquet(chunk['data_path'], engine='pyarrow')
 
-            # TODO: here we should build a filter for those DataFrames in
-            # order to remove all rows that are not inline with the `spec`.
+            for f in spec['filters']:
+                chunk_df = chunk_df[
+                    operators_to_def[f['operator']](chunk_df[f['name']], f['value'])
+                ]
             chunk_dfs.append(chunk_df)
 
-        return pd.concat(chunk_dfs)
+        return pd.concat(chunk_dfs)[spec['columns']]
 
     # def discover(self):
     #     from IPython.display import IFrame
